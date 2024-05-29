a2cd6edea082465493c25cf10eeebd69cef7f3f2
king-ak@github.com <king-ak@github.com>
2024-05-29 19:45:36 +0000

    Update Notebook

--- notebooks/bronze/ingest.py
+++ notebooks/bronze/ingest.py
@@ -1,28 +1,20 @@
 # Databricks notebook source
-"""
-This notebook ingests data from blob storage
-"""
+# MAGIC %md
+# MAGIC # Bronze Ingestion
+# MAGIC This notebook ingests data from the landing zone in blob storage and creates a Bronze table
 
+# COMMAND ----------
+
+# Widgets
 dbutils.widgets.text("storage_account_name", "")
 dbutils.widgets.text("container_name", "")
+dbutils.widgets.text("bronze_database_name", "")
 
 
 storage_account_name = dbutils.widgets.get("storage_account_name")
 container_name = dbutils.widgets.get("container_name")
-database_name = "bronze_db"
+database_name = dbutils.widgets.get("bronze_database_name")
 table_name = "raw_nfl_play_by_play_data"
-
-# COMMAND ----------
-
-# # Example of how to set up config had cluster not been configured to use secrets
-# app_id = dbutils.secrets.get(scope=secret_scope,key="sp-databricks-poc-app-id")
-# service_credential = dbutils.secrets.get(scope=secret_scope, key="sp-databricks-poc-app-secret")
-
-# spark.conf.set(f"fs.azure.account.auth.type.{storage_account_name}.dfs.core.windows.net", "OAuth")
-# spark.conf.set(f"fs.azure.account.oauth.provider.type.{storage_account_name}.dfs.core.windows.net", "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider")
-# spark.conf.set(f"fs.azure.account.oauth2.client.id.{storage_account_name}.dfs.core.windows.net", app_id)
-# spark.conf.set(f"fs.azure.account.oauth2.client.secret.{storage_account_name}.dfs.core.windows.net", service_credential)
-# spark.conf.set(f"fs.azure.account.oauth2.client.endpoint.{storage_account_name}.dfs.core.windows.net", f"https://login.microsoftonline.com/{tenant_id}/oauth2/token")
 
 # COMMAND ----------
 
@@ -31,16 +23,9 @@ dbutils.fs.ls(f"abfss://{container_name}@{storage_account_name}.dfs.core.windows
 
 # COMMAND ----------
 
-# Read CSV file, persist as 
+# Read CSV file
 df = spark.read.format("csv").option("header","true").load(f"abfss://{container_name}@{storage_account_name}.dfs.core.windows.net/raw/data/NFL Play by Play 2009-2018 (v5).csv")
 display(df)
-
-# COMMAND ----------
-
-# Create external database in storage account
-spark.sql(f"""CREATE SCHEMA IF NOT EXISTS {database_name}
-             LOCATION 'abfss://{container_name}@{storage_account_name}.dfs.core.windows.net/{database_name}' 
-             """)
 
 # COMMAND ----------
 
